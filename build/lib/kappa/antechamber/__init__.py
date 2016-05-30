a1066c52e73be02cd86cd0214a9ca36560243642
Alex Kerr <ajkerr0@gmail.com>
2016-05-29 19:58:16 -0500

    Remove build directory

--- build/lib/kappa/antechamber/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Apr 29 13:37:49 2016
-
-@author: alex
-"""
-
--- build/lib/kappa/lattice/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Mar 22 11:21:16 2016
-
-@author: alex
-"""
-
