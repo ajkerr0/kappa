3840fefa9d6b4f0dd65adfe081d6dea93f00bc35
Alex Kerr <ajkerr0@gmail.com>
2016-06-04 19:40:41 -0500

    Add Green's function coefficient calculation

--- kappa/greens_func.py
+++ kappa/greens_func.py
@@ -16,14 +16,13 @@ def main():
     k0 = 1.
     m0 = 1.
     gamma0 = 0.1
-    N = 6
+    N = 3
     
     #get neighbor lists
     nLists = find_neighbors(N)
     
     #get Kmatrix (N x N)
     KMatrix = calculate_K_matrix(N,k0,nLists)
-    print KMatrix
     
     #get gamma matrix (N x N)
     gammaMatrix = calculate_gamma_matrix(N,gamma0)
@@ -35,20 +34,59 @@ def main():
     #2N eigenvalues: N lambdha and N lambdha*
     #2N eigenvectors of length 2N
     val,vec = calculate_evec(MMatrix,gammaMatrix,KMatrix)
-
-#    print val
-#    print vec
-#    print np.shape(vec)
     
-def calculate_green_function(vec):
-    """Return the Green function"""
+    #testing with dummy inputs
+#    i,j = 2, 2
+#    x = (2*MMatrix[i,i]*val[j] + gammaMatrix[i,i])*vec[i,j]
+#    print x
+
+    coeff = calculate_greens_function(val, vec, MMatrix, gammaMatrix)
+    
+    pos = integrate_system(coeff, val, vec)
+    
+def integrate_system(coeff, val, vec):
+    
+    N = len(val)/2
+    
+    #starting at zero
+    t0 = 0.
+    tf = 10.
+    n = 1
+    t = np.linspace(t0, tf, n)
+    
+    #force
+#    F = np.matrix(np.zeros(N))
+    
+    for step in t:
+        arg = val*step
+        expMat = np.exp(np.matrix(np.diag(arg))*np.matrix(np.ones((6,6))))
+        gFunc = np.matrix(vec[:N,:])*expMat*coeff
+        print gFunc
+    
+        
+    
+    
+def calculate_greens_function(val, vec, massMat, gMat):
+    """Return the 2N x N Green's function coefficient matrix."""
     
     N = len(vec)/2
-    print N
-    gfxn = np.zeros(N,dtype=complex)
     
+    #need to determine coefficients in eigenfunction/vector expansion
+    # need linear solver to solve equations from notes
+    # AX = B where X is the matrix of expansion coefficients
     
-    return gfxn
+    A = np.zeros((2*N, 2*N), dtype=complex)
+    A[:N,:] = vec[:N,:]
+
+    #adding mass and damping terms to A
+    lamda = np.tile(val, (N,1))
+
+    A[N:,:] = np.multiply(A[:N,:], 2*massMat*lamda + gMat*np.ones((N,2*N)))
+    
+    #now prep B
+    B = np.concatenate((np.zeros((N,N)), np.identity(N)), axis=0)
+
+    return np.linalg.solve(A,B)
     
 def calculate_evec(M,G,K):
     
@@ -63,11 +101,6 @@ def calculate_evec(M,G,K):
     x = np.concatenate((x,np.zeros([N,N])),axis=1)
     y = np.concatenate((np.zeros([N,N]),-M),axis=1)
     z = np.matrix(np.concatenate((x,y),axis=0))
-    
-    #testing validity of numpy method so we wouldn't need SciPy
-#    w, v = np.linalg.eig(np.linalg.inv(z)*c)
-#    print w
-#    print v
     
     w,vr = linalg.eig(c,b=z,right=True)
     
