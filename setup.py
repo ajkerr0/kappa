ed7db13d41ea76ffcdf619e4586257872e75f4b4
Alex Kerr <ajkerr0@gmail.com>
2016-06-09 13:20:29 -0500

    Configure setup.py to run parameter parsers
    Add the instructions to run the forcefield parameter file parsers at installation to generate the .npy files in the /param/ subdirectories.  Also standardize the name of the parser module.  Delete the current .npy files.

--- setup.py
+++ setup.py
@@ -4,8 +4,44 @@ Created on Thu May 26 14:19:51 2016
 
 @author: Alex Kerr
 """
-
+import os,sys
 from setuptools import find_packages, setup
+from setuptools.command.install import install
+from setuptools.command.develop import develop
+
+def parse(dir_):
+    from subprocess import call
+    #get path to interpreter
+    interpreter = sys.executable
+    #get param directories
+    param_dir = 'kappa/param/'
+    parser_paths = [path for path in os.listdir(param_dir) if os.path.isdir(os.path.join(param_dir, path))]
+    for path in parser_paths:
+        call([interpreter, "parser.py"],
+             cwd=os.path.join(dir_, path))
+
+#Peter Lamut: http://blog.niteoweb.com/setuptools-run-custom-code-in-setup-py/
+def customcmd(command_subclass):
+    """A decorator for subclasses of the setuptools commands, 
+    modifying the run() method so that it parses the force parameter
+    files after installation."""
+    
+    orig_run = command_subclass.run
+    
+    def modified_run(self):
+        orig_run(self)
+        self.execute(parse, (self.install_lib,))
+        
+    command_subclass.run = modified_run
+    return command_subclass
+        
+@customcmd
+class CustomInstallCommand(install):
+    pass
+
+@customcmd
+class CustomDevelopCommand(develop):
+    pass
 
 setup(name='kappa',
       version='0.2.1',
@@ -17,7 +53,11 @@ setup(name='kappa',
       install_requires=[
       'numpy',
       'matplotlib',
-      'scipy'
+      'scipy',
       ],
+      cmdclass={
+      'install': CustomInstallCommand,
+      'develop': CustomDevelopCommand,
+      },
       )
 
