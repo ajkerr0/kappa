15378f74d9b5b0be19bc32c1a9b7f502b1530c8c
Alex Kerr <ajkerr0@gmail.com>
2016-07-14 16:13:01 -0500

    Add a .top file generating function
    Add a function in md/generation.py that creates .top list to be put in a file via generation.save_file.  It is incomplete now: not only is it missing entries but the _build_lines function needs to be finished to remove duplicate code.

--- kappa/md/generate.py
+++ kappa/md/generate.py
@@ -1,9 +1,12 @@
-import elements as ELEMENTS
+import numpy as np
+
+from . import elements as ELEMENTS
 
 
 def pdb(molecule):
-    # Creates a .pdb (protein data bank) type list for use with molecular dynamics packages
-    # pdb_lines returns list holding every line of .pdb file
+    """Creates a .pdb (protein data bank) type list for use with molecular dynamics packages.
+    pdb_lines returns list holding every line of .pdb file."""
+    
     pdb_lines = []
     atom_lines = []
     conect_lines = []
@@ -56,7 +59,86 @@ def pdb(molecule):
     pdb_lines.append("TER")
     pdb_lines.append("END")
     return pdb_lines
-
+    
+def top(mol):
+    """Creates a .top (topology) type list for use in MD packages"""
+    
+    top_lines = [";", ";     Topology file for %s" % mol.name, ";"]
+    
+    #defaults line
+    #used to establish default parameters (for when they aren't specified)????
+    #copy and pasting from example file
+    top_lines.extend(["[ defaults ]",
+                      "; nbfunc        comb-rule       gen-pairs       fudgeLJ fudgeQQ",
+                      "  1             1               no              1.0     1.0",
+                      ""])
+    
+    #inclue forcefield field file
+    top_lines.extend(["; The force field files to be included",
+                      '#include "%s.itp"' % mol.ff.name,
+                      ""])
+                      
+    #ignoring moleculetype
+    #maybe add a molecule type attribute to our molecules?
+    
+    #atoms
+    top_lines.append("[ atoms ]")
+#    top_lines.extend(_build_lines(["nr", "type", "cgnr", "charge"],
+#                                  5,
+#                                  len(mol),
+#                                  map(str, [range(len(mol)), mol.atomtypes, 
+#                                       np.ones(len(mol), dtype=int), np.zeros(len(mol), dtype=float)])))
+    columns = ["nr", "type", "cgnr", "charge"]
+    spaces = 5
+    line = ";"
+    for column in columns:
+        line += ' '*spaces
+        line += column
+    top_lines.append(line)
+    for i in range(len(mol)):
+        atomcols = map(str, [i, mol.atomtypes[i], 1, 0.000])
+        line = ' '
+        for count, col in enumerate(atomcols):
+            entryLength = spaces+len(columns[count])
+            line += col.rjust(entryLength, ' ')
+        top_lines.append(line)
+    top_lines.append(" ")
+        
+    #bonds
+    top_lines.append("[ bonds ]")
+    columns = ["ai", "aj", "funct", "c0", "c1"]
+    spaces = 12
+    line = ";"
+    for column in columns:
+        line += ' '*spaces
+        line += column
+    top_lines.append(line)
+    for m, bond in enumerate(mol.bondList):
+        bondcols = map(str, [bond[0], bond[1], 1, mol.kb[m], mol.b0[m]])
+        line = " "
+        for count, col in enumerate(bondcols):
+            entryLength = spaces+len(columns[count])
+            line += col.rjust(entryLength, ' ')
+        top_lines.append(line)
+            
+    return top_lines
+    
+def _build_lines(columns, spaces, size, innerColumns):
+    listSection = []
+    line = ";"
+    for column in columns:
+        line += ' '*spaces
+        line += column
+    listSection.append(line)
+    for i in range(size):
+        line = ' '
+        for count, col in enumerate([x[i] for x in innerColumns]):
+            entryLength = spaces+len(columns[count])
+            line += col.rjust(entryLength, ' ')
+        listSection.append(line)
+    listSection.append(' ')
+    return listSection
+    
 
 def save_file(txt_object, save_dir, name):
     f = open(save_dir + "/%s.pdb" % name, 'w')
