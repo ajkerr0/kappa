c2119869bcb5788dfc528efb2df8368cb42c14b7
Alex Kerr <ajkerr0@gmail.com>
2016-08-08 11:29:49 -0500

    Add add method to Calculationused to create a trial molecule in which to calculate its thermal conductivity

--- kappa/conductivity.py
+++ kappa/conductivity.py
@@ -12,3 +12,18 @@ class Calculation:
             self.base = base
         else:
             raise ValueError("A base molecule with 2 interfaces is needed!")
+        self.trialList = []
+            
+    def add(self, molList, indexList):
+        """Append a trial molecule to self.trialList with enhancements 
+        from molList attached to atoms in indexList"""
+        
+        from .operation import _combine
+        for mol, index in zip(molList,indexList):
+            for count, face in enumerate(self.faces):
+                if index in face.atoms:
+                    iface = count
+            newTrial,_ = _combine(self, mol, index, 0, 0, iface, 0, copy=False)
+        newTrial._configure()
+        self.trialList.append(newTrial)
+        return newTrial
