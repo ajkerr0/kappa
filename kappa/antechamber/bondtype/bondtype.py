c3d18801a958e23e4a1323987199ed420c452c2a
Alex Kerr <ajkerr0@gmail.com>
2016-08-24 17:50:54 -0500

    Fix recursive function to return all valence states of a tps
    Begin the bond order assignment function (boaf) detailed in the same paper.

--- kappa/antechamber/bondtype/bondtype.py
+++ kappa/antechamber/bondtype/bondtype.py
@@ -15,7 +15,7 @@ def main(mol):
     """Return the perceived bond types list for `mol`,
     indexed like `mol.bondList`."""
     
-    max_valence_state = 2000
+    max_valence_state = 200
     state_num = 0
     
     tps = 0
@@ -23,30 +23,39 @@ def main(mol):
     #first we need to numerate the possible valences and respective penalty scores for each atom
     av = find_atomic_valences(mol)
     
-    vstates = np.array([])
+    vstates = np.array([np.zeros(len(mol), dtype=int)])
     
     while state_num < max_valence_state:
         
         #find states of given tps
         newstates = bondtype(tps, av)
         
-        vstates = np.concatenate((vstates, newstates),axis=0)
+        print(tps)
+        print(newstates)
+        if newstates is not None:
+            vstates = np.concatenate((vstates, newstates),axis=0)
+            num = len(newstates)
+        else:
+            num = 0
         
         #increment tps and state number
         tps += 1
-        state_num += len(newstates)
+        state_num += num
         
-    for vstate in vstates:
-        
-        match, b_order = boaf(vstate)
-        
-        if match:
-            break
-        
-    #develop bond types from bond order
-    b_types = b_order
-        
-    return b_types
+#    for vstate in vstates[1:]:
+#        
+#        match, b_order = boaf(vstate, mol.bondList)
+#        
+#        if match:
+#            break
+#    else:
+#        raise ValueError("Valid bond order assignments were not found. \
+#                          Consider increasing the max valance state count")
+#        
+#    #develop bond types from bond order
+#    b_types = b_order
+#        
+#    return b_types
     
 def bondtype(tps, av):
     """Return all the combinations of valence states for the given tps."""
@@ -56,21 +65,68 @@ def bondtype(tps, av):
     def dfs(index, vstate, runsum):
         """A recursive function to fill `vstates` will all valence states
         of total penalty score `tps`"""
-        newstate = vstate[:]
-        try:
-            for valence, ps in av[index]:
-                if ps+runsum <= tps:
-                    dfs(index+1, newstate, runsum+ps)
-        except IndexError:
-            vstates.append(newstate)
+        for valence, ps in av[index]:
+            newstate = vstate[:]
+            newsum = runsum
+            if ps+runsum <= tps:
+                newstate.append(valence)
+                newsum += ps
+#                print("{0},{1}".format(newstate, newsum))
+                if len(newstate) < len(av):
+                    dfs(index+1, newstate, newsum)
+                elif len(newstate) == len(av):
+                    if newsum == tps:
+                        vstates.append(newstate)
+            
+    dfs(0, [], 0)
     
-    return np.array(vstates)
+    if len(vstates) == 0:
+        return None
+    else:
+        return np.array(vstates)
     
-def boaf(vstate):
+def boaf(vstate, bondList):
     """Return True if bond order assignment of the valence state is successful,
     otherwise return False."""
     
-    return False, None
+    #connectivity and bond order lists
+    con0 = np.bincount(bondList[:,0])
+    con1 = np.bincount(bondList[:,1])
+    l0, l1 = len(con0), len(con1)
+    if l0 > l1:
+        con1 = np.concatenate((con1, np.zeros(l0-l1, dtype=int)))
+    elif l1 > l0:
+        con0 = np.concatenate((con0, np.zeros(l1-l0, dtype=int))) 
+    conList = con0 + con1
+    boList = [None]*len(bondList)
+    
+    print(conList)
+            
+    #2: set the orders of remaining bonds to 1 if con == av
+    #3: set the orders to av if con == 1
+    for i in range(len(vstate)):
+        if conList[i] == vstate[i]:
+            bonds = np.where(bondList==i)[0]
+            for j in bonds:
+                boList[j] = 1
+            
+        elif conList[i] == 1:
+            bonds = np.where(bondList==i)[0]
+            boList[np.where(bondList==i)[0][0]] = vstate[i]
+    
+    
+    return True, None
+    
+def rule1_adjustment(bonds, bondList, bo, conList, vstate):
+    """Change the connectivity and atomic valence lists based on Rule1 
+    of 'boaf'"""
+    
+    #1: if order is determined reduced the valence and connectivity
+    for i,j in bondList[bonds]:
+        conList[i] += -1
+        conList[j] += -1
+        vstate[i] += -bo
+        vstate[j] += -bo
     
 def find_atomic_valences(mol):
     
@@ -84,7 +140,6 @@ def find_atomic_valences(mol):
         #there must be a better way to do this
         lines.append(line)
     
-    print(lines)
     av = []
     
     #for each atom in the molecule search through the APS file and add all possible valence values
