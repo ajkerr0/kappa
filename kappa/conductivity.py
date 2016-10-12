97bc400954b2dc88cacc939648a5c5fcd45b308d
Alex Kerr <ajkerr0@gmail.com>
2016-10-12 15:13:55 -0500

    Refactor t. conductivity code
    Remove all use of ballandspring Hessians.  Compile all inspection functions into a single 'inspect' function to remove the clutter from the main routine.  Remove obsolete functions 'w_to_text' and 'find_indices'.  Move inspect functions towards bottom of file to keep them separated from the main function.  Remove everything related to the characteristic equation because that wasn't well formulated to begin with.

--- kappa/conductivity.py
+++ kappa/conductivity.py
@@ -4,9 +4,8 @@
 @author: Alex Kerr
 
 """
-import pprint
+
 import itertools
-import random
 from copy import deepcopy
 
 import numpy as np
@@ -126,10 +125,6 @@ def calculate_thermal_conductivity(mol, driverList, baseSize, gamma):
     
     from .operation import _calculate_hessian
     kMatrix = _calculate_hessian(mol, stapled_index, numgrad=False)
-    ballandspring=False
-#    print(kMatrix)
-#    kMatrix = hessian(mol)
-#    kMatrix, ballandspring = _calculate_ballandspring_k_mat(len(mol), 1., mol.nList), True
     
     gMatrix = _calculate_gamma_mat(len(mol), gamma, driverList)
     
@@ -151,9 +146,6 @@ def calculate_thermal_conductivity(mol, driverList, baseSize, gamma):
         interactions = mol.angleList
     elif mol.ff.lengths:
         interactions = mol.bondList
-        
-    if ballandspring:
-        interactions = mol.bondList
 
     for it in interactions:
         for atom in atoms0:
@@ -170,49 +162,33 @@ def calculate_thermal_conductivity(mol, driverList, baseSize, gamma):
                     crossings.append([element, atom])
                     
     #add nonbonded interactions
+    ''' to 
+        be 
+        completed '''
                     
     #remove duplicate interactions
     crossings.sort()
-    crossings = list(k for k,_ in itertools.groupby(crossings))    
-                
-    kappa = 0.
-    
+    crossings = list(k for k,_ in itertools.groupby(crossings))
     print(crossings)
+         
+    #initialize the thermal conductivity value
+    kappa = 0.
     
 #    mullenTable = []
     mullenTable = None
-    
-#    inspect_char_equation(gamma, kMatrix, driverList)
-#    inspect_orthogonality(vec, kMatrix)
-#    print_spring_constants(mol, crossings, kMatrix)
-#    inspect_positive_definiteness(kMatrix)
-#    inspect_space_homogeneity(crossings, kMatrix)
-#    inspect_symmetry(kMatrix)
-#    inspect_nondecay_modes(mol, val, vec)
-    inspect_com_modes(mol, vec)
                 
     for crossing in crossings:
         i,j = crossing
         kappa += _calculate_power(i,j,val, vec, coeff, kMatrix, driverList, mullenTable)
 #        kappa += _calculate_power_loop(i,j,val, vec, coeff, kMatrix, driverList, mullenTable)
     
+    inspect(mol, val, vec, kMatrix, crossings, mullenTable)
+
+#    import pprint
 #    pprint.pprint(mullenTable)
-#    inspect_term_modes(mol, mullenTable, vec)
-#    print(len(mullenTable))
     print(kappa)
     return kappa, None
 #    return kappa, mullenTable
-    
-    
-    
-   ########
-    #searching for weird discriminant values
-#    for k in mullenTable:
-#        print(gamma)
-#        print(k)
-#        print(np.sqrt(complex(gamma**2 - 4.*k)))
-#        
-#    return kappa
     
 def _calculate_power_loop(i,j, val, vec, coeff, kMatrix, driverList, mullenTable):
     
@@ -287,139 +263,6 @@ def _calculate_power(i,j, val, vec, coeff, kMatrix, driverList, mullenTable):
                 kappa += np.sum(termArr)
                 
     return kappa
-    
-def print_spring_constants(mol, interactions, kmat):
-    
-#    acts = np.array(interactions)
-#    
-#    for act in acts:
-#        i,j = act
-#        print(act)
-#        print(kmat[3*i:3*i+3,3*j:3*j+3])
-        
-#    print('random diagonal blocks')
-    diags = [0,11,20]
-    for diag in diags:
-        print('atom {0}'.format(diag))
-        print(kmat[3*diag:3*diag+3, 3*diag:3*diag+3])
-        _slice = mol.hessian_routine_analytical(diag)
-        print(_slice)
-        print(_slice/kmat[3*diag:3*diag+3, 3*diag:3*diag+3])
-        
-                
-        
-        
-def inspect_positive_definiteness(kmat):
-    
-    atom = 20
-    hill = 1e-3
-    
-    kmat[atom*3  ,atom*3  ] += hill
-    kmat[atom*3+1,atom*3+1] += hill
-    kmat[atom*3+2,atom*3+2] += hill
-    
-    try:
-        np.linalg.cholesky(kmat)
-        print("hessian is positive definite")
-    except np.linalg.LinAlgError:
-        print("hessian is NOT positive definite")
-        
-    val , vec = np.linalg.eig(kmat)
-    
-    print(val)
-        
-def inspect_symmetry(kmat):
-    
-    print("symmetry: ")
-    print(np.allclose(kmat.transpose(), kmat))
-        
-def inspect_space_homogeneity(interactions, kmat):
-    
-    acts = np.array(interactions)
-    
-    for act in acts:
-        i,j = act
-        print(np.sum(kmat[3*i:3*i+3]))
-        print(np.sum(kmat[3*j:3*j+3]))
-        
-def inspect_nondecay_modes(mol, val, vec):
-    
-    #find where modes are uncoupled from dissapation
-    uncoupled = np.where(np.real(val) > -1e-12)[0]
-    
-    N = len(vec)//2
-    
-    #pick an index
-#    index = 0
-#    index = uncoupled[index]
-    
-    from .plot import normal_modes
-    for index in uncoupled[:6]:
-        normal_modes(mol, vec[:N,index])
-        
-def inspect_term_modes(mol, mullenTable, vec):
-    """Plot mode pairs that correspond to high thermal conductivity contributions"""
-    
-    N = len(vec)//2
-    
-    from .plot import normal_modes
-    
-    for row in mullenTable[:5]:
-        sigma, tau = row[1], row[2]
-        normal_modes(mol, vec[:N, sigma], track=row[3:5])
-        normal_modes(mol, vec[:N, tau], track=row[3:5])
-        
-def inspect_com_modes(mol, vecs):
-    """Raise issue if normal mode shifts center of mass."""
-    
-    N = len(vecs)//2
-    nmol = len(mol)
-    
-    com = mol.com()
-    
-    for vec in np.transpose(vecs[:N]):
-        vec = vec.reshape(nmol,3)
-        
-        mol.posList += 100.*vec.real
-        new_com = mol.com()
-        mol.posList += -100.*vec.real
-        
-        print(new_com-com)
-    
-        
-def inspect_orthogonality(vec, kmat):
-    
-    
-#    val, vec = np.linalg.eig(kmat)
-#    vec = np.imag(vec)
-    
-    N = len(vec)
-    
-    for i in range(N):
-        for j in range(N):
-            
-#            if abs(j-i) > 1:
-#                
-#                dot = np.dot(vec[:,i], vec[:,j])
-#                
-#                if abs(dot) > 1e-2:
-#                    print(dot)
-            print(i)
-            print(j)
-            print(np.dot(vec[:,i], vec[:,j]))
-                
-    print("that was ortho check")
-    
-def inspect_char_equation(g, kmat, driverList):
-    """Print values from the characteristic equation."""
-    
-    drivers = driverList[1]
-    
-    for driver in drivers:
-        print(driver)
-        print(g)
-        print(kmat[driver,driver])
-        print(np.sqrt(complex(g**2 - 4.*kmat[driver,driver])))
     
 def _calculate_coeff(val, vec, massMat, gMat):
     """Return the 2N x N Green's function coefficient matrix."""
@@ -500,25 +343,131 @@ def _calculate_ballandspring_k_mat(N,k0,nLists):
     
     return KMatrix
     
-def find_indices(base, num):
+def inspect(mol, val, vec, kMatrix, crossings, mullenTable):
+
+#    inspect_orthogonality(vec, kMatrix)
+#    inspect_spring_constants(mol, crossings, kMatrix)
+#    inspect_positive_definiteness(kMatrix)
+#    inspect_space_homogeneity(crossings, kMatrix)
+#    inspect_symmetry(kMatrix)
+#    inspect_nondecay_modes(mol, val, vec)
+#    inspect_com_modes(mol, vec)
+#    inspect_term_modes(mol, mullenTable, vec)
+    pass
     
-    indices = []
+def inspect_spring_constants(mol, interactions, kmat):
     
-    if num > 1:
-        #select indices that are most 'spread out'
-        pass
-    elif num == 1:
-        #pick a random index
-        for faceindex in [0,1]:
-            face = base.faces[faceindex]
-            choices = [x for x in face.atoms if x not in face.closed]
-            indices.append(random.choice(choices))
-    else:
-        raise ValueError("Number of chains must be an integer greater than zero")
+#    acts = np.array(interactions)
+#    
+#    for act in acts:
+#        i,j = act
+#        print(act)
+#        print(kmat[3*i:3*i+3,3*j:3*j+3])
         
-    return indices
+#    print('random diagonal blocks')
+    diags = [0,11,20]
+    for diag in diags:
+        print('atom {0}'.format(diag))
+        print(kmat[3*diag:3*diag+3, 3*diag:3*diag+3])
+        _slice = mol.hessian_routine_analytical(diag)
+        print(_slice)
+        print(_slice/kmat[3*diag:3*diag+3, 3*diag:3*diag+3])
+        
+def inspect_positive_definiteness(kmat):
     
-def write_to_txt(twodlist, name):
-    with open(name,'w') as f:
-        for x in zip(*twodlist):
-            f.write("{0}\t{1}\t{2}\t{3}\t{4}\n".format(*x))
+    atom = 20
+    hill = 1e-3
+    
+    kmat[atom*3  ,atom*3  ] += hill
+    kmat[atom*3+1,atom*3+1] += hill
+    kmat[atom*3+2,atom*3+2] += hill
+    
+    try:
+        np.linalg.cholesky(kmat)
+        print("hessian is positive definite")
+    except np.linalg.LinAlgError:
+        print("hessian is NOT positive definite")
+        
+    val , vec = np.linalg.eig(kmat)
+    
+    print(val)
+        
+def inspect_symmetry(kmat):
+    
+    print("symmetry: ")
+    print(np.allclose(kmat.transpose(), kmat))
+        
+def inspect_space_homogeneity(interactions, kmat):
+    
+    acts = np.array(interactions)
+    
+    for act in acts:
+        i,j = act
+        print(np.sum(kmat[3*i:3*i+3]))
+        print(np.sum(kmat[3*j:3*j+3]))
+        
+def inspect_nondecay_modes(mol, val, vec):
+    
+    #find where modes are uncoupled from dissapation
+    uncoupled = np.where(np.real(val) > -1e-12)[0]
+    
+    N = len(vec)//2
+    
+    #pick an index
+#    index = 0
+#    index = uncoupled[index]
+    
+    from .plot import normal_modes
+    for index in uncoupled[:6]:
+        normal_modes(mol, vec[:N,index])
+        
+def inspect_term_modes(mol, mullenTable, vec):
+    """Plot mode pairs that correspond to high thermal conductivity contributions"""
+    
+    N = len(vec)//2
+    
+    from .plot import normal_modes
+    
+    for row in mullenTable[:5]:
+        sigma, tau = row[1], row[2]
+        normal_modes(mol, vec[:N, sigma], track=row[3:5])
+        normal_modes(mol, vec[:N, tau], track=row[3:5])
+        
+def inspect_com_modes(mol, vecs):
+    """Raise issue if normal mode shifts center of mass."""
+    
+    N = len(vecs)//2
+    nmol = len(mol)
+    
+    com = mol.com()
+    
+    for vec in np.transpose(vecs[:N]):
+        vec = vec.reshape(nmol,3)
+        
+        mol.posList += 100.*vec.real
+        new_com = mol.com()
+        mol.posList += -100.*vec.real
+        
+        print(new_com-com)
+        
+def inspect_orthogonality(vec, kmat):
+    
+#    val, vec = np.linalg.eig(kmat)
+#    vec = np.imag(vec)
+    
+    N = len(vec)
+    
+    for i in range(N):
+        for j in range(N):
+            
+#            if abs(j-i) > 1:
+#                
+#                dot = np.dot(vec[:,i], vec[:,j])
+#                
+#                if abs(dot) > 1e-2:
+#                    print(dot)
+            print(i)
+            print(j)
+            print(np.dot(vec[:,i], vec[:,j]))
+                
+    print("that was ortho check")
