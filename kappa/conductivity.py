ec0eb5de22b571d49798345ea5da13aeafef0a60
Alex Kerr <ajkerr0@gmail.com>
2016-11-21 17:52:05 -0600

    Begin plot_contributions, a 3d plot to characterize the terms in the best tcond contributions
    Also reflect the fact that kappaList now holds dictionaries in ModeInspector.plot_ppation

--- kappa/conductivity.py
+++ kappa/conductivity.py
@@ -171,7 +171,7 @@ class ModeInspector(Calculation):
         max_indices = []
         for entry in kappaList:
             #get the sigma, tau indices
-            max_indices.extend(entry[0:2])
+            max_indices.extend([entry['sigma'], entry['tau']])
             
         max_indices = np.array(max_indices)
         
@@ -192,6 +192,24 @@ class ModeInspector(Calculation):
         plt.plot(np.real(val), np.imag(val), 'bo')
         
         fig.suptitle("Re[val] vs Im[val]")
+        
+        plt.show()
+        
+    def plot_contributions(self):
+        
+        kappa, kappaList,_,_ = self.tcond()
+        
+        size = len(kappaList)
+        val = np.zeros((size, 3))
+        
+        for index, entry in enumerate(kappaList):
+            val[index] = entry['kappa'], entry['val_num'], 1./entry['val_den']
+         
+        fig = plt.figure()         
+         
+        plt.plot(val[:,0], val[:,1], val[:,2])
+        
+        fig.suptitle("Distribution of max kappa contributions")
         
         plt.show()
         
