Correctly scale the dihedral interaction strengths, fix issue where multiple Fourier orders were not being considered, remove unphysical units from mix2.
Include negative sign in Fourier order parsing and correctly scale dihedral interaction strength with divider

Correctly handle negative Fourier order from the dihedral parameter parsing

Remove unphysical units from PolymerMix2

These are atomistic configurations that do not exist in nature due to the competition of electron-withdrawing species.

Add missing factor of one-half to dihedral energy (and derivative) calculations

kappa/molecule.py
@@ -309,7 +309,7 @@ class Molecule:
                 dihArr, vnArr = np.load(filename+'/dih.npy'), np.load(filename+'/vn.npy')
                 dihedrals = np.transpose([idList[self.dihList[:,0]], idList[self.dihList[:,1]],
                                           idList[self.dihList[:,2]], idList[self.dihList[:,3]]])
-            
+
                 def store_dih_param(index, paramList, vnList):
                     vns = vnList[index]
                     paramList[0][abs(int(vns[2]))-1] = vns[0]
@@ -317,7 +317,7 @@ class Molecule:
                     if vns[2] >= 0.:
                         return paramList
                     else:
-                        store_dih_param(index+1, paramList, vnList)
+                        return store_dih_param(index+1, paramList, vnList)
                         
                 valuesList = []
                 #find indices of wildcards in dihArr
@@ -501,7 +501,7 @@ class Molecule:
                 return np.sum(self.vn[:,0]*(1. + np.cos(np.radians(   omega - self.gn[:,0])))
                             + self.vn[:,1]*(1. + np.cos(np.radians(2.*omega - self.gn[:,1])))
                             + self.vn[:,2]*(1. + np.cos(np.radians(3.*omega - self.gn[:,2])))
-                            + self.vn[:,3]*(1. + np.cos(np.radians(4.*omega - self.gn[:,3]))))
+                            + self.vn[:,3]*(1. + np.cos(np.radians(4.*omega - self.gn[:,3]))))/2.
                 
             e_funcs.append(e_imptors)
             
@@ -636,7 +636,7 @@ class Molecule:
                 uTerm = (    self.vn[:,0]*np.sin(np.radians(   omega - self.gn[:,0]))
                         + 2.*self.vn[:,1]*np.sin(np.radians(2.*omega - self.gn[:,1]))
                         + 3.*self.vn[:,2]*np.sin(np.radians(3.*omega - self.gn[:,2]))
-                        + 4.*self.vn[:,3]*np.sin(np.radians(4.*omega - self.gn[:,3])))
+                        + 4.*self.vn[:,3]*np.sin(np.radians(4.*omega - self.gn[:,3])))/2.
                 dudri = uTerm[:,None]*dwdri
                 dudrj = uTerm[:,None]*dwdrj
                 dudrk = uTerm[:,None]*dwdrk
@@ -1466,16 +1466,8 @@ _mix2_array = np.array([
                       [[9],[9]],
                       [[17],[1]],
                       [[1],[17]],
-                      [[17],[9]],
-                      [[9],[17]],
-                      [[17],[17]],
                       [[35],[1]],
                       [[1],[35]],
-                      [[35],[9]],
-                      [[9],[35]],
-                      [[35],[17]],
-                      [[17],[35]],
-                      [[35],[35]]
                       ])
           
 def build_mix(ff, idList):

kappa/operation.py
@@ -274,15 +274,15 @@ def hess_dihedral(pos, i, j, k, l, vn, gn):
     term1 += -(1./rkj/rkj)[:,None]*poslk
     jj += np.einsum('ki,kj->kij', term1, dwdrl)
     
-    u2 = -(  vn[:,0]*np.sin(np.radians(   omega - gn[:,0]))
-        + 2.*vn[:,1]*np.sin(np.radians(2.*omega - gn[:,1]))
-        + 3.*vn[:,2]*np.sin(np.radians(3.*omega - gn[:,2]))
-        + 4.*vn[:,3]*np.sin(np.radians(4.*omega - gn[:,3])))
+    u2 = -.5*(  vn[:,0]*np.sin(np.radians(   omega - gn[:,0]))
+           + 2.*vn[:,1]*np.sin(np.radians(2.*omega - gn[:,1]))
+           + 3.*vn[:,2]*np.sin(np.radians(3.*omega - gn[:,2]))
+           + 4.*vn[:,3]*np.sin(np.radians(4.*omega - gn[:,3])))
     
-    u1 = -(   vn[:,0]*np.cos(np.radians(   omega - gn[:,0]))
-        +  4.*vn[:,1]*np.cos(np.radians(2.*omega - gn[:,1]))
-        +  9.*vn[:,2]*np.cos(np.radians(3.*omega - gn[:,2]))
-        + 16.*vn[:,3]*np.cos(np.radians(4.*omega - gn[:,3])))
+    u1 = -.5*(   vn[:,0]*np.cos(np.radians(   omega - gn[:,0]))
+           +  4.*vn[:,1]*np.cos(np.radians(2.*omega - gn[:,1]))
+           +  9.*vn[:,2]*np.cos(np.radians(3.*omega - gn[:,2]))
+           + 16.*vn[:,3]*np.cos(np.radians(4.*omega - gn[:,3])))
     
     ii = u2[:,None,None]*ii + u1[:,None,None]*np.einsum('ki,kj->kij', dwdri, dwdri)
     ji = u2[:,None,None]*ji + u1[:,None,None]*np.einsum('ki,kj->kij', dwdrj, dwdri)

kappa/param/amber99/parser.py
@@ -77,8 +77,7 @@ def parse():
         line=line[0]
         a,b,c,d = map(str.rstrip, [line[:2],line[3:5],line[6:8],line[9:11]])
         dih_types.append([a,b,c,d])
-        vns.append([float(line[18:24]),float(line[31:38]),float(line[49:54])])
-        
+        vns.append([float(line[18:24])/float(line[14:15]),float(line[31:38]),float(line[48:54])])
     dihArr = [[None if i == wc else a_types.index(i) for i in dih_type] for dih_type in dih_types]
     np.save("dih", dihArr)
     np.save("vn", vns)
@@ -96,8 +95,6 @@ def parse():
     for line in lines[section[6]+2:section[7]-2]:
         line = line[0]
         line = line.split()
-        print(line)
-        print(line[0])
         vdw_type = a_types.index(line[0])
         for dup in vdw_dups:
             if line[0] == dup[0]:
@@ -109,10 +106,6 @@ def parse():
             else:
                 eps[vdw_type] = float(line[2])
                 rvdw0[vdw_type] = float(line[1])
-    
-    print(a_types)          
-    print(eps)
-    print(rvdw0)
         
     np.save("epvdw", eps)
     np.save("rvdw0", rvdw0)